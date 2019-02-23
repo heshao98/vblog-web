d6c00b5b9746736cba369c9097c33595205801d8
He-Polaris <779326052@qq.com>
2019-02-23 22:09:26 +0800

    测试

--- vblog-web/src/api/upload.js
+++ vblog-web/src/api/upload.js
@@ -3,7 +3,7 @@ import request from '@/request'
 export function upload(formdata) {
   return request({
     headers: {'Content-Type': 'multipart/form-data'},
-    url: '/upload',
+    url: '/u pload',
     method: 'post',
     data: formdata
   })
